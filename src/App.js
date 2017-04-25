a2c84937e6d622d1bbbcf515250c12e89c9ffede
ddsheard <ddsheard@gmail.com>
2017-04-24 21:55:50 -0400

    almost fixed

--- src/App.js
+++ src/App.js
@@ -18,30 +18,61 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      text: ''
+      text: '',
+      photoAdded: false
     }
   }
 
   handleChange (event) {
     console.log(event.target.value)
     console.log(this.state)
-    this.setState({ text: event.target.value })
+    this.setState({ text: event.target.value });
   }
 
-  disableCheck () {
-    return this.state.text.length === 0
+  togglePhoto (event) {
+    this.setState({ photoAdded: !this.state.photoAdded});
+  }
+
+  overflowAlert() {
+    if (this.remainingCharacters() < 0) {
+      if (this.state.photoAdded) {
+        var beforeOverflowText = this.state.text.substring(140 - 23 - 10, 140 - 23);
+        var overflowText = this.state.text.substring(140 - 23);
+      } else {
+        var beforeOverflowText = this.state.text.substring(140 - 10, 140);
+        var overflowText = this.state.text.substring(140);
+      }
+      return (
+        <div className="alert alert-warning">
+        <strong>Oops! Too Long:</strong>
+        &nbsp;...{beforeOverflowText}
+        <strong className="bg-danger">{overflowText}</strong>
+        </div>
+      );
+    } else {
+        return "";
+    }
+  }
+
+  remainingCharacters () {
+    if (this.state.photoAdded) {
+      return 140 - 23 - this.state.text.length;
+    } else {
+      return 140 - this.state.text.length;
+    }
   }
 
   render() {
     return (
       <div className="well clearfix">
-        <textarea
-          onChange={this.handleChange.bind(this)}
-          className="form-control"></textarea>
+          { this.overflowAlert() }
+        <textarea className="form-control"></textarea>
         <br/>
-        <span>{140 - this.state.text.length}</span>
-        <button className="btn btn-primary pull-right" disabled={this.disableCheck()}>Tweet</button>
-        <p>{this.state.text}</p>//this isnt necessary tyo have as text is repeated
+        <span>{this.remainingCharacters()}</span>
+        <button className="btn btn-primary pull-right" disabled={this.state.text.length === 0 && !this.state.photoAdded}>Tweet</button>
+        <button className="btn btn-default pull-right" onClick={this.togglePhoto}>
+          {this.state.photoAdded ? "✓  Photo Added" : "Add Photo"}
+        </button>
       </div>
     );
   }
